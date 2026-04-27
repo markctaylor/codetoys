f8cd73501af46f331e48bd9e43f9d7857b10efcf
markctaylor <taylorconsulting@indigoskies.com>
2026-04-26 22:02:32 -0500

    added some more explanatory doc

--- idfactory/src/idfactory/UniqueTimestampGeneratorTest.java
+++ idfactory/src/idfactory/UniqueTimestampGeneratorTest.java
@@ -16,13 +16,16 @@ import org.junit.jupiter.api.Test;
 
 class UniqueTimestampGeneratorTest {
 
+	// This accumulates all IDs generated across all tests so id reuse (collisions) can be detected at the end
 	static Map<Long, Integer> countMap;
 	
 	@BeforeAll
 	static void setupOnce() {
 	    countMap = new HashMap<>(100000);
 	}
-	
+
+	// this AfterAll method is actually a test itself, looking through the accumulated IDs
+	// for any that were seen more than once.
 	@AfterAll
 	static void summarizeAndAssertOverallSuccessCriteria() {
 		System.out.println("Looking for collisions");
@@ -33,12 +36,22 @@ class UniqueTimestampGeneratorTest {
 	}
 
 	@Test
+	void testOne() {
+		// this is the entire calling convention for the unique timestamp/long/ID generation utility
+		long ts = UniqueTimestampGenerator.get();
+		
+		assertTrue(ts > 0, "A value was generated");
+	}
+	
+	@Test
 	void testLinearHighSpeedAllocation() {
 		long last, ts = 0;
 		
 		for (int i=0; i<10000; i++) {
 			last = ts;
 			ts = UniqueTimestampGenerator.get();
+			
+			// accumulates the ID for later collision detection
 			countMap.merge(ts, 1, Integer::sum);
 
 			assertTrue(ts>last, "new ts should always be greater than previous ts");
@@ -50,6 +63,8 @@ class UniqueTimestampGeneratorTest {
 	void testThreadedHighSpeedAllocation() throws InterruptedException, ExecutionException {
 		System.out.println("Multi-threaded test started at: "+System.currentTimeMillis());
 
+		// execute 5 copies of the linear test concurrently to encourage any thread race conditions / collisions.
+		// note that all assertions for this test occur either in the runnable or in the AfterAll sweep
 		ExecutorService executor = Executors.newFixedThreadPool(5);
         Future<?> future1 = executor.submit(()-> {testLinearHighSpeedAllocation();} );
         Future<?> future2 = executor.submit(()-> {testLinearHighSpeedAllocation();} );
